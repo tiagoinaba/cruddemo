3ec5bdc66d8921b6808fb55092f4e7b198c8c9ad
tiagoinaba <tiago.inaba@gmail.com>
2023-06-18 09:34:33 -0300

    Added springdoc-openapi

--- src/main/java/com/tkinaba/solr/cruddemo/CruddemoApplication.java
+++ src/main/java/com/tkinaba/solr/cruddemo/CruddemoApplication.java
@@ -1,13 +1,17 @@
 package com.tkinaba.solr.cruddemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Employee CRUD with SolrCloud",
+		description = "REST API that provides CRUD services to use with SolrCloud.",
+		version = "1.0"))
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
-
 }
